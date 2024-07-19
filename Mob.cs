badc2fef0b834837e037ed713f258d8633659bcd
Filipe <sogarnsystems@gmail.com>
2024-07-19 11:16:59 -0400

    Added mob and changed file names

---
+++ Mob.cs
@@ -0,0 +1,39 @@
+using Godot;
+
+public partial class Mob : CharacterBody3D
+{
+	// Minimum speed of mob in meters per second
+	[Export]
+	public int MinSpeed { get; set; } = 10;
+	// Max speed
+	[Export]
+	public int MaxSpeed { get; set; } = 18;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		MoveAndSlide();
+	}
+
+	// Will be called from main scene
+	public void Initialize(Vector3 startPosition, Vector3 playerPosition)
+	{
+		// We position the mob by placing it at startPosition
+		// and rotate it towards playerPosition so it looks at the player
+		LookAtFromPosition(startPosition, playerPosition, Vector3.Up);
+		// Rotate this mob randomly within +- 45 degrees so it doesn't move directly towards the player
+		RotateY((float)GD.RandRange(-Mathf.Pi / 4.0, Mathf.Pi / 4.0));
+
+		// Calculate a random speed
+		int randomSpeed = GD.RandRange(MinSpeed, MaxSpeed);
+		// Calculate forward velocity
+		Velocity = Vector3.Forward * randomSpeed;
+		// Rotate the velocity vector based on the mob's Y rotation to move in the direction it is looking
+		Velocity = Velocity.Rotated(Vector3.Up, Rotation.Y);
+	}
+
+	// On leaving the screen delete the mob
+	private void OnVisibleOnScreenNotifier3dScreenExited()
+	{
+		QueueFree();
+	}
+}
--- player.cs
+++ player.cs
@@ -1,62 +1,61 @@
 using Godot;
-using System;
 
-public partial class player : CharacterBody3D
+public partial class Player : CharacterBody3D
 {
-    // Speed of player
-    [Export]
-    public int Speed { get; set; } = 14;
+	// Speed of player
+	[Export]
+	public int Speed { get; set; } = 14;
 
-    // Downward acceleration while in the air
-    [Export]
-    public int FallAcceleration { get; set; } = 75;
+	// Downward acceleration while in the air
+	[Export]
+	public int FallAcceleration { get; set; } = 75;
 
-    private Vector3 _targetVelocity = Vector3.Zero;
+	private Vector3 _targetVelocity = Vector3.Zero;
 
-    public override void _PhysicsProcess(double delta)
-    {
-        // Local variable to store input direction
-        var direction = Vector3.Zero;
+	public override void _PhysicsProcess(double delta)
+	{
+		// Local variable to store input direction
+		var direction = Vector3.Zero;
 
-        // Check for each move input and update direction
-        if (Input.IsActionPressed("move_right"))
-        {
-            direction.X += 1.0f;
-        }
-        if (Input.IsActionPressed("move_left"))
-        {
-            direction.X -= 1.0f;
-        }
-        // Worth noting in 3D XZ are the ground plane
-        if (Input.IsActionPressed("move_back"))
-        {
-            direction.Z += 1.0f;
-        }
-        if (Input.IsActionPressed("move_forward"))
-        {
-            direction.Z -= 1.0f;
-        }
+		// Check for each move input and update direction
+		if (Input.IsActionPressed("move_right"))
+		{
+			direction.X += 1.0f;
+		}
+		if (Input.IsActionPressed("move_left"))
+		{
+			direction.X -= 1.0f;
+		}
+		// Worth noting in 3D XZ are the ground plane
+		if (Input.IsActionPressed("move_back"))
+		{
+			direction.Z += 1.0f;
+		}
+		if (Input.IsActionPressed("move_forward"))
+		{
+			direction.Z -= 1.0f;
+		}
 
-        // Normalize vector
-        if (direction != Vector3.Zero)
-        {
-            direction = direction.Normalized();
-            // Basis sets the rotation
-            GetNode<Node3D>("Pivot").Basis = Basis.LookingAt(direction);
-        }
+		// Normalize vector
+		if (direction != Vector3.Zero)
+		{
+			direction = direction.Normalized();
+			// Basis sets the rotation
+			GetNode<Node3D>("Pivot").Basis = Basis.LookingAt(direction);
+		}
 
-        // Ground velocity
-        _targetVelocity.X = direction.X * Speed;
-        _targetVelocity.Z = direction.Z * Speed;
+		// Ground velocity
+		_targetVelocity.X = direction.X * Speed;
+		_targetVelocity.Z = direction.Z * Speed;
 
-        // Vertical velocity if in air
-        if (!IsOnFloor())
-        {
-            _targetVelocity.Y -= FallAcceleration * (float)delta;
-        }
+		// Vertical velocity if in air
+		if (!IsOnFloor())
+		{
+			_targetVelocity.Y -= FallAcceleration * (float)delta;
+		}
 
-        // Moving the character
-        Velocity = _targetVelocity;
-        MoveAndSlide();
-    }
+		// Moving the character
+		Velocity = _targetVelocity;
+		MoveAndSlide();
+	}
 }
